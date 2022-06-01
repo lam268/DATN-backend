d753c0876ce016cc7870b25a01f0f6eb580bef5a
lam268 <lamlv@tokyotechlab.com>
2022-06-01 15:29:42 +0700

    fix cron

--- src/modules/timekeeping/timekeeping.module.ts
+++ src/modules/timekeeping/timekeeping.module.ts
@@ -4,7 +4,7 @@ import { TimekeepingService } from './services/timekeeping.service';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from '../user/services/user.service';
 import { DatabaseService } from 'src/common/services/database.service';
-// import { ReadFingerScannerDataJob } from './cron-job/readFingerScannerData.job';
+import { ReadFingerScannerDataJob } from './cron-job/readFingerScannerData.job';
 import { RequestAbsenceService } from '../request-absence/services/requestAbsence.service';
 import { ExportExcelService } from '../timekeeping/services/export-excel.service';
 import { HolidayService } from '../setting/services/holiday.service';
@@ -12,8 +12,8 @@ import { ContractTypeService } from '../setting/services/contract-type.service';
 import { UserTimekeepingHistoryService } from './services/userTimekeepingHistory.service';
 import { CreateUserTimekeepingHistoryJob } from './cron-job/createUserTimekeepingHistory.job';
 import { ContractService } from '../contract/services/contract.service';
-// import { DownloadFingerScannerDataJob } from './cron-job/downloadFingerScannerData.job';
-// import { DeleteFingerScannerDataFilesJob } from './cron-job/deleteFingerScannerDataFiles.job';
+import { DownloadFingerScannerDataJob } from './cron-job/downloadFingerScannerData.job';
+import { DeleteFingerScannerDataFilesJob } from './cron-job/deleteFingerScannerDataFiles.job';
 import { SettingService } from '../setting/services/setting.service';
 @Module({
     imports: [ConfigService],
@@ -25,14 +25,14 @@ import { SettingService } from '../setting/services/setting.service';
         ExportExcelService,
         HolidayService,
         DatabaseService,
+        ReadFingerScannerDataJob,
         ContractTypeService,
         UserTimekeepingHistoryService,
         SettingService,
         CreateUserTimekeepingHistoryJob,
         ContractService,
-        // ReadFingerScannerDataJob,
-        // DownloadFingerScannerDataJob,
-        // DeleteFingerScannerDataFilesJob,
+        DownloadFingerScannerDataJob,
+        DeleteFingerScannerDataFilesJob,
     ],
 })
 export class TimekeepingModule {}
